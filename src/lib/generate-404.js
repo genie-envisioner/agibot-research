60ae6f9b8d6ef849441aa2296dfc7a650860e0fb
yangdonglin <yangdonglin@buaa.edu.cn>
2025-08-12 01:46:48 +0800

    init

---
+++ src/lib/generate-404.js
@@ -0,0 +1,19 @@
+
+const fs = require('fs')
+const path = require('path')
+
+const repoName = 'agibot-research' 
+const distDir = path.resolve(__dirname, '../dist')
+const filePath = path.join(distDir, '404.html')
+
+const html = `<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="refresh" content="0; url=/${repoName}/" />
+  </head>
+  <body></body>
+</html>
+`
+
+fs.writeFileSync(filePath, html, 'utf-8')
+console.log('✅ 404.html created at', filePath)
